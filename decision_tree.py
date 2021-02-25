c3df8882dc8e93bc5fa6cbddae5cc69a9ab09963
sallykim515 <39283556+sallykimhk@users.noreply.github.com>
2021-02-25 14:40:12 -0800

    Code refactoring
    Move support functions out to decision_tree_functions.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,68 +1,25 @@
 ###########################################################
 # re-do of the class project in Python, following Kaggle's micro course on "Intro to Machine Learning"
 ###########################################################
+import decision_tree_functions as fn
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
-import matplotlib.pyplot as plt
 
 # import data
 vanc_data = pd.read_csv("House sale data Vancouver.csv")
 y = vanc_data.Price
 X = vanc_data[["Total floor area", "Lot Size", "Age"]]  # vanc_data.drop(["Price", "Address", "List Date"], axis=1)
 
-# out-of-sample
+# out of sample
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
-# build the model
-vanc_model = DecisionTreeRegressor(random_state=1)
-vanc_model.fit(train_X, train_y)
-
-# predict
-predictions = vanc_model.predict(val_X)
-
-# validate
+# default max_leaf_nodes
+val_y, predictions = fn.build_and_predict(train_X, val_X, train_y, val_y, max_leaf_nodes=None, random_state=1)
 print("%% Average MAE of default Decision Tree: \t\t\t %.2f %%" % (mean_absolute_error(val_y, predictions) / np.mean(y) * 100))
 
-
-###########################################################
-# under- vs. over- fitting
-# code adopted from https://www.kaggle.com/dansbecker/underfitting-and-overfitting
-###########################################################
-
-
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=1)
-    model.fit(train_X, train_y)
-    preds = model.predict(val_X)
-    mae = mean_absolute_error(val_y, preds)
-    return (mae)
-
-
-nodes = []
-maes = []
-for max_leaf_node in range(2, 100, 1):
-    nodes.append(max_leaf_node)
-    maes.append(get_mae(max_leaf_node, train_X, val_X, train_y, val_y))
-
-# visualization of mae
-fig, ax = plt.subplots()
-ax.plot(nodes, maes)
-ax.set(title='MAE vs. Max # of Leaf Nodes',
-       xlabel='Max # of Leaf Nodes',
-       ylabel='MAE')
-plt.grid(True, linestyle='--')
-plt.show()
-
 # re-build the model using optimal # of leaves
-best_leaf_num = nodes[maes.index(min(maes))]
-vanc_model2 = DecisionTreeRegressor(random_state=1, max_leaf_nodes=best_leaf_num)
-vanc_model2.fit(train_X, train_y)
-
-# predict
-predictions2 = vanc_model2.predict(val_X)
-
-# validate
+best_leaf_num = fn.viz_maes_and_opt_leaf(train_X, val_X, train_y, val_y)
+val_y, predictions2 = fn.build_and_predict(train_X, val_X, train_y, val_y, max_leaf_nodes=best_leaf_num, random_state=1)
 print("%% Average MAE of Decision Tree w/ optimal depth: \t %.2f %%" % (mean_absolute_error(val_y, predictions2) / np.mean(y) * 100))
---
+++ decision_tree_functions.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+
+
+# code adopted from https://www.kaggle.com/dansbecker/underfitting-and-overfitting
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=1)
+    model.fit(train_X, train_y)
+    preds = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds)
+    return (mae)
+
+
+def build_and_predict(train_X, val_X, train_y, val_y, max_leaf_nodes, random_state):
+
+    # build the model
+    vanc_model = DecisionTreeRegressor(random_state=1)
+    vanc_model.fit(train_X, train_y)
+
+    # predict
+    predictions = vanc_model.predict(val_X)
+
+    return val_y, predictions
+
+
+# visualize maes & return optimal # of leaf nodes
+def viz_maes_and_opt_leaf(train_X, val_X, train_y, val_y):
+    nodes = []
+    maes = []
+    for max_leaf_node in range(2, 100, 1):
+        nodes.append(max_leaf_node)
+        maes.append(get_mae(max_leaf_node, train_X, val_X, train_y, val_y))
+
+    fig, ax = plt.subplots()
+    ax.plot(nodes, maes)
+    ax.set(title='MAE vs. Max # of Leaf Nodes',
+           xlabel='Max # of Leaf Nodes',
+           ylabel='MAE')
+    plt.grid(True, linestyle='--')
+    plt.show()
+
+    return nodes[maes.index(min(maes))]
