e02bfc9d2497a3ed400d0dff0cb4a2518bb9ecb5
sallykim515 <39283556+sallykimhk@users.noreply.github.com>
2021-02-26 12:35:15 -0800

    Code refactoring
    Removed redundancy in support functions

--- decision_tree.py
+++ decision_tree.py
@@ -5,21 +5,22 @@ import decision_tree_functions as fn
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
 
 # import data
 vanc_data = pd.read_csv("House sale data Vancouver.csv")
 y = vanc_data.Price
-X = vanc_data[["Total floor area", "Lot Size", "Age"]]  # vanc_data.drop(["Price", "Address", "List Date"], axis=1)
+X = vanc_data[["Total floor area", "Lot Size", "Age"]]
 
-# out of sample
+# split training and validation sets
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
 # default max_leaf_nodes
-val_y, predictions = fn.build_and_predict(train_X, val_X, train_y, val_y, max_leaf_nodes=None, random_state=1)
-print("%% Average MAE of default Decision Tree: \t\t\t %.2f %%" % (mean_absolute_error(val_y, predictions) / np.mean(y) * 100))
+model = fn.build_decision_tree(train_X, train_y, max_leaf_nodes=None, random_state=1)
+mae = fn.get_mae(model, val_X, val_y)
+print("%% Average MAE of default Decision Tree: \t\t\t %.2f %%" % (mae / np.mean(y) * 100))
 
-# re-build the model using optimal # of leaves
+# re-build the model using optimal # of leaf nodes
 best_leaf_num = fn.viz_maes_and_opt_leaf(train_X, val_X, train_y, val_y)
-val_y, predictions2 = fn.build_and_predict(train_X, val_X, train_y, val_y, max_leaf_nodes=best_leaf_num, random_state=1)
-print("%% Average MAE of Decision Tree w/ optimal depth: \t %.2f %%" % (mean_absolute_error(val_y, predictions2) / np.mean(y) * 100))
+model2 = fn.build_decision_tree(train_X, train_y, max_leaf_nodes=best_leaf_num, random_state=1)
+mae2 = fn.get_mae(model2, val_X, val_y)
+print("%% Average MAE of Decision Tree w/ optimal depth: \t %.2f %%" % (mae2 / np.mean(y) * 100))
--- decision_tree_functions.py
+++ decision_tree_functions.py
@@ -1,30 +1,18 @@
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
 
-# code adopted from https://www.kaggle.com/dansbecker/underfitting-and-overfitting
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=1)
+def build_decision_tree(train_X, train_y, max_leaf_nodes=None, random_state=1):
+    model = DecisionTreeRegressor(random_state=random_state, max_leaf_nodes=max_leaf_nodes)
     model.fit(train_X, train_y)
+    return model
+
+
+def get_mae(model, val_X,  val_y):
     preds = model.predict(val_X)
     mae = mean_absolute_error(val_y, preds)
-    return (mae)
-
-
-def build_and_predict(train_X, val_X, train_y, val_y, max_leaf_nodes, random_state):
-
-    # build the model
-    vanc_model = DecisionTreeRegressor(random_state=random_state, max_leaf_nodes=max_leaf_nodes)
-    vanc_model.fit(train_X, train_y)
-
-    # predict
-    predictions = vanc_model.predict(val_X)
-
-    return val_y, predictions
+    return mae
 
 
 # visualize maes & return optimal # of leaf nodes
@@ -33,7 +21,8 @@ def viz_maes_and_opt_leaf(train_X, val_X, train_y, val_y):
     maes = []
     for max_leaf_node in range(2, 100, 1):
         nodes.append(max_leaf_node)
-        maes.append(get_mae(max_leaf_node, train_X, val_X, train_y, val_y))
+        model=build_decision_tree(train_X, train_y, max_leaf_node, random_state=1)
+        maes.append(get_mae(model, val_X, val_y))
 
     fig, ax = plt.subplots()
     ax.plot(nodes, maes)
